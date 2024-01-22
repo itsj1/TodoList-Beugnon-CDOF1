01e5d8606bbe12d1329770e232adb186288309b8
Jérémy <jeremy.beugnon@edu.devinci.fr>
2024-01-22 14:29:52 +0100

    test

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+setup(
+ name='TodoList-Beugnon-CDOF1',
+ version='1.0',
+ author='itsj1',
+ license='MIT',
+ long_description=open('README.md').read(),
+)
